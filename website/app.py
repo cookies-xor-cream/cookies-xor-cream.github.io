1cb149d5831899730af15f595928adc217a15152
Arithmetic-Overflow <aryageza@gmail.com>
2021-07-13 17:17:22 +0800

    Added a colour palette and linked the css/js to the base template

--- website/app.py
+++ website/app.py
@@ -2,5 +2,9 @@ from flask import render_template
 from website import app
 
 @app.route('/')
-def hello():
+def index():
     return render_template("overview/overview.html")
+
+@app.route('/favicon.ico')
+def favicon():
+    return ''
