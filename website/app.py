f443a07c6c82c8b78088bd39527921aa32b65e36
Arithmetic-Overflow <aryageza@gmail.com>
2021-07-13 22:54:55 +0800

    Added favicon

--- website/app.py
+++ website/app.py
@@ -1,10 +1,29 @@
-from flask import render_template
+import os
+from flask import render_template, url_for, send_from_directory
 from website import app
-
-@app.route('/')
-def index():
-    return render_template("overview/overview.html")
 
 @app.route('/favicon.ico')
 def favicon():
-    return ''
+	return send_from_directory(os.path.join(app.root_path, 'static'),
+		'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/')
+@app.route('/home')
+def index():
+	return render_template("overview/overview.html")
+
+@app.route('/projects')
+def projects():
+	return 'projects'
+
+@app.route('/contact')
+def contact():
+	return 'contact'
+
+@app.route('/about')
+def about():
+	return 'about'
+
+@app.route('/resume')
+def resume():
+	return 'resume'
