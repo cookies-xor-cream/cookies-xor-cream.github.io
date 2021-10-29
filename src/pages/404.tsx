a785d9263eb4437d2acc257eb80c364be558dc96
Arya Gerami Zadegan <aryageza@gmail.com>
2021-10-29 19:05:50 +0800

    added custom text to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,8 +6,8 @@ import Seo from "../components/seo"
 const NotFoundPage: React.FC = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404: Page Not Found</h1>
+    <h3 style={{textAlign: "center"}}>May I suggest next time you try looking harder...<br /> <i>for content that does exist</i></h3>
   </Layout>
 )
 
