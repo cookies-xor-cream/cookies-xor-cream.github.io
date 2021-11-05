d1b0fbc0067a5b0147310b959f62a1570148d829
Arya Gerami Zadegan <aryageza@gmail.com>
2021-11-05 14:58:01 +0800

    Added repo name to gh pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: `/personal-website`,
   siteMetadata: {
     title: `cookies-xor-cream`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
