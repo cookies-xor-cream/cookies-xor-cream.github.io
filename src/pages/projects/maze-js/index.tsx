63232aca6efe3b75a8ace609b7f468ea42182f17
Arya Gerami Zadegan <aryageza@gmail.com>
2021-11-05 15:07:13 +0800

    Fixed navigate function to only run in browser environment

--- src/pages/projects/maze-js/index.tsx
+++ src/pages/projects/maze-js/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { navigate } from 'gatsby-link'
 
 const GraphicsEngine = () => {
-    navigate('/projects/project-not-implemented');
+    useEffect(() => {
+        navigate('/projects/project-not-implemented');
+    }, []);
     return null;
 }
 
