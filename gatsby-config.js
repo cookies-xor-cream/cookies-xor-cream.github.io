92fd5821424b659cb060c9f95fb107173293441d
Arya Gerami Zadegan <aryageza@gmail.com>
2021-11-05 16:34:59 +0800

    Removed relative path and renamed repo... hopefully fixes pathing problems

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  pathPrefix: '/personal-website',
   siteMetadata: {
     title: `cookies-xor-cream`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
