3f92be9f96243089dfeffcd226295af193a3e8c4
Arya Gerami Zadegan <aryageza@gmail.com>
2021-11-05 15:10:44 +0800

    GH pages config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/personal-website`,
+  pathPrefix: '/reponame',
   siteMetadata: {
     title: `cookies-xor-cream`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
