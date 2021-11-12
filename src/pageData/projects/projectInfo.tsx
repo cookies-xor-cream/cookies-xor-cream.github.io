f7aa7e14911ec598cbe8d899d09b32d3bc0d3818
cookies-xor-cream <aryageza@gmail.com>
2021-11-12 14:57:05 +0800

    Added a tag system similar to that of technologies or prerequisites

--- src/pageData/projects/projectInfo.tsx
+++ src/pageData/projects/projectInfo.tsx
@@ -1,3 +1,5 @@
+import tagdata from "./tagdata";
+
 import BoidVid from "/src/videos/boids_demo.webm";
 import QuadVid from "/src/videos/artorias_quadtree.webm";
 import GraphicsVid from "/src/videos/cat_graphics_demo.webm";
@@ -44,41 +46,35 @@ export default [
       vid: DFTVid,
       title: "DFT",
       description: "Graphs the contours of an image via epicycles by using the discrete Fourier transform.",
-      tags: [
-        {name: "Image Processing", color: "blue"},
-        { name: "Graph Theory", color: "purple" }
-      ],
+      tags: [tagdata.imageprocessing],
       href: "/projects/dft-sketch",
     },
     {
       vid: QuadVid,
       title: "Quadtree Compression",
       description: "Compresses an image by recursively decomposing areas of high deviation into four smaller sections.",
-      tags: [
-        { name: "Interactive", color: "pink" },
-        { name: "simulation", color: "green" }
-      ],
+      tags: [tagdata.imageprocessing],
       href: "/projects/quadtree",
     },
     {
       vid: BoidVid,
       title: "Boids",
       description: "Simulates bird-like or fish-like behaviours through three simple rules: separation, alignment, and cohesion. <br/><br/> An example of emergent behaviours.",
-      tags: [],
+      tags: [tagdata.simulation],
       href: "/projects/boids",
     },
     {
       vid: MazeGenVid,
       title: "Maze Generator/Solver",
       description: "A program that generates and solves mazes using a variety of seemingly unrelated graph algorithms.",
-      tags: [],
+      tags: [tagdata.graphtheory],
       href: "/projects/maze-js",
     },
     {
       vid: GraphicsVid,
       title: "Graphics Engine",
       description: "A rudimentary graphics engine that runs solely on 1 CPU thread, written to practice the mathematical foundations of graphics programming.",
-      tags: [],
+      tags: [tagdata.interactive],
       href: "/projects/graphics-engine",
       },
   ]
---
+++ src/pageData/projects/tagdata.tsx
@@ -0,0 +1,6 @@
+export default {
+    imageprocessing:    { name: "image processing", color: "blue" },
+    graphtheory:        { name: "graph theory", color: "purple" },
+    interactive:        { name: "interactive", color: "pink" },
+    simulation:         { name: "simulation", color: "green" }
+}
