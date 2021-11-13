28f8bda56f8c7cb663818795a2c5d0f28dcbf434
Arya Gerami Zadegan <aryageza@gmail.com>
2021-11-13 10:06:22 +0800

    Updated videos (and every other field) to purely use the embedded md data

---
+++ src/pageData/projects/videos.tsx
@@ -0,0 +1,15 @@
+import BoidVid from "/src/videos/boids_demo.webm";
+import QuadVid from "/src/videos/artorias_quadtree.webm";
+import GraphicsVid from "/src/videos/cat_graphics_demo.webm";
+import MazeGenVid from "/src/videos/mazejs_gen.webm";
+import MazeSolveVid from "/src/videos/mazejs_solve.webm";
+import DFTVid from "/src/videos/wile_dft.webm";
+
+export default {
+	BoidVid,
+	QuadVid,
+	GraphicsVid,
+	MazeGenVid,
+	MazeSolveVid,
+	DFTVid
+}
