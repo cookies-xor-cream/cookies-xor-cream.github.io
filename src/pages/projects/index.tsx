6cd3770fe154a20209836b749e14bb45921327bd
Arya Gerami Zadegan <aryageza@gmail.com>
2021-11-13 10:47:55 +0800

    Made projects page dynamic with md files (but videos fail)

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { graphql } from "gatsby";
 
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 import ProjectCardGrid from "../../components/project-card-grid";
 
 import projectInfo from "../../pageData/projects/projectInfo.tsx";
+import videos from "../../pageData/projects/videos.tsx";
 
 const SecondPage: React.FC = () => (
   <Layout currentPage="projects">
@@ -18,4 +19,43 @@ const SecondPage: React.FC = () => (
   </Layout>
 )
 
-export default SecondPage
+const IndexPage = ({
+  data: { allMarkdownRemark: { nodes } }
+}) => {
+  const projectData = nodes.map(data => {return {
+    href: data.frontmatter.slug,
+    title: data.frontmatter.title,
+    description: data.frontmatter.carddesc,
+    tags: data.frontmatter.tags,
+    vid: videos[data.frontmatter.videoName]
+  }})
+
+  console.log("???", projectData);
+
+  return (
+    <Layout currentPage="projects">
+      <Seo title="projects" />
+      <h1>Projects</h1>
+      <h3>
+        Click on a card to read its related post, the source code for most of these projects is available on <a href="https://github.com/cookies-xor-cream/">my Github</a>
+      </h3>
+      <ProjectCardGrid projectDetailsList={projectData} />
+    </Layout>
+  );
+}
+export default IndexPage
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          title
+          carddesc
+          videoName
+          tags
+        }
+      }
+    }
+  }
+`
