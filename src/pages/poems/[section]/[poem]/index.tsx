92de4ebc7540558a91b87b78584a3e5b84da820a
cookies-xor-cream <aryageza@gmail.com>
2023-02-18 12:55:43 +0800

    Scaffolding for adding poems

---
+++ src/pages/poems/[section]/[poem]/index.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Test = (props) => <div> Test: {props.params.section} : {props.params.poem} </div>
+
+export default Test;
---
+++ src/pages/poems/[section]/[poem]/{Poems.frontmatter__slug}.tsx
@@ -0,0 +1,78 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "/src/components/layout"
+import Seo from "/src/components/seo";
+
+import * as styles from "/src/pages/projects/project-styles.module.scss";
+import "katex/dist/katex.min.css"
+
+import InfoGrid from "/src/components/projects/info-grid";
+
+import techlinks from "/src/pageData/projects/techlinks";
+import prereqs from "/src/pageData/projects/prereqs";
+import videos from "/src/pageData/projects/videos";
+
+import ProjectNotFound from "/src/pages/projects/project-not-implemented";
+
+export default function ProjectPost({ data }) {
+  const rawData = data.markdownRemark;
+
+  const postData = {
+      title: rawData.frontmatter.title,
+      video: videos[rawData.frontmatter.videoName],
+      overview: rawData.frontmatter.overview,
+      prereqs: rawData.frontmatter.prereqs.map(prereq => prereqs[prereq]),
+      techStack: rawData.frontmatter.techStack.map(tech => techlinks[tech]),
+      difficulty: rawData.frontmatter.difficulty,
+      timeest: rawData.frontmatter.timeest,
+      __html: rawData.html,
+      tableOfContents: rawData.tableOfContents,
+      githubLink: rawData.frontmatter.githubLink,
+  };
+
+  if(postData.overview === "") {
+    return <ProjectNotFound />
+  }
+
+  return (
+      <Layout currentPage="projects">
+          <Seo/>
+          <h1>{postData.title}</h1>
+
+          <InfoGrid
+              vidSrc={postData.video}
+              timeEstimate={postData.timeest}
+              difficulty={postData.difficulty}
+              overview={postData.overview}
+              techStack={postData.techStack}
+              prereqs={postData.prereqs}
+              githubLink={postData?.githubLink}
+          />
+
+          <div
+              className={styles.projectPost}
+              dangerouslySetInnerHTML={postData}
+          />
+      </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        overview
+        title
+        prereqs
+        difficulty
+        techStack
+        timeest
+        videoName
+        githubLink
+      }
+      html
+      tableOfContents
+    }
+  }
+`
